a6a6aaa9df5a5f1adf55aa53b19990d43c5591c8
Kalana Vimukthi Weerarathna <kalana.weerarathne@scicom.com.my>
2020-06-09 23:52:51 +0530

    init

---
+++ abdelboutar-service/src/main/java/com/abdelboutar/abdelboutarservice/AbdelboutarServiceApplication.java
@@ -0,0 +1,13 @@
+package com.abdelboutar.abdelboutarservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AbdelboutarServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AbdelboutarServiceApplication.class, args);
+    }
+
+}
---
+++ abdelboutar-service/src/test/java/com/abdelboutar/abdelboutarservice/AbdelboutarServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abdelboutar.abdelboutarservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AbdelboutarServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
